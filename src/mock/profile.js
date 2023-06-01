7fdc7aac0fdedcf68e110fb87d6e5e8cf3f0d2c1
smurf-Azrael <phillwang117@gmail.com>
2023-06-01 16:11:56 +0900

    portfolio'

--- src/mock/profile.js
+++ src/mock/profile.js
@@ -7,11 +7,11 @@ export const SOCIAL_LINKS = [
 
 ];
 
-export const HERO_TITLES = ['Phill Lin', 'Full-stack Developer', 'Blockchain Developer'];
+export const HERO_TITLES = ['Luis Velasco', 'Full-stack Developer', 'Blockchain Developer'];
 
 export const aboutParagraph =
   'Highly motivated and enthusiastic Full Stack Developer with experience in designing, developing and maintaining web applications using technologies such as JavaScript, React, Node.js. ';
 
-export const contactEmail = 'philljohnson739@gmail.com';
+export const contactEmail = 'luisvelasco00117@gmail.com';
 
-export const documentTitle = 'Hello I am Phill Lin | Full Stack Developer | Blockchain Developer'
+export const documentTitle = 'Hello I am Luis Velasco | Full Stack Developer | Blockchain Developer'
